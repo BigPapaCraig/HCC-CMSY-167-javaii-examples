5f7d53f52ffcafec3cbad8c0ee1d5d9bd3636130
Chris Finley <cfinley@howardcc.edu>
2021-03-01 20:01:32 -0500

    Start Collections demos.

---
+++ collections/src/main/java/edu/howardcc/javaii/examples/ListDemo.java
@@ -0,0 +1,36 @@
+package edu.howardcc.javaii.examples;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Demonstrate some basic operations on a List
+ */
+public class ListDemo {
+
+    public static void main(String[] args) {
+
+        // Create and populate a List
+        List<String> authors = new ArrayList<>(List.of("Larson", "Tolkien", "Dubner", "Rothfuss", "Gaiman", "Rowling"));
+
+        // Print some info about the List
+        System.out.println("Contents:" + authors);
+        System.out.println("Size: " + authors.size());
+        System.out.println("First element: " + authors.get(0));
+        System.out.println("Fourth element: " + authors.get(3));
+        System.out.println("Index of \"Dubner\": " + authors.indexOf("Dubner"));
+
+        // Remove an element
+        System.out.println("\nRemoving \"Rothfuss\"");
+        authors.remove("Rothfuss");
+        System.out.println("Contents:" + authors);
+        System.out.println("Size: " + authors.size());
+
+        // Clear the list (remove everything)
+        System.out.println("\nClearing the list");
+        authors.clear();
+        System.out.println("Contents:" + authors);
+        System.out.println("Size: " + authors.size());
+    }
+
+}
---
+++ collections/src/main/java/edu/howardcc/javaii/examples/ListPopulation.java
@@ -0,0 +1,54 @@
+package edu.howardcc.javaii.examples;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Demonstrate different ways of populating a list
+ */
+public class ListPopulation {
+
+    public static void main(String[] args) {
+
+        System.out.println("Create mutable list and add elements: " + createListAndAddElements());
+        System.out.println("Create immutable list with Arrays.asList(): " + createListWithArraysAsList());
+        System.out.println("Create immutable list with List.of(): " + createListWithListOf());
+        System.out.println("Create mutable list with List.of(): " + createMutableListWithListOf());
+
+    }
+
+    /**
+     * Create a new list and populate it one element at a time
+     */
+    private static List<String> createListAndAddElements() {
+        List<String> list = new ArrayList<>();
+        list.add("Larson");
+        list.add("Tolkien");
+        list.add("Dubner");
+        list.add("Rothfuss");
+        return list;
+    }
+
+    /**
+     * Create an immutable(not modifiable) list using Arrays.asList (JDK 8+)
+     */
+    private static List<String> createListWithArraysAsList() {
+        return Arrays.asList("Larson", "Tolkien", "Dubner", "Rothfuss");
+    }
+
+    /**
+     * Create an immutable(not modifiable) list using List.of (JDK 9+)
+     */
+    private static List<String> createListWithListOf() {
+        return List.of("Larson", "Tolkien", "Dubner", "Rothfuss");
+    }
+
+    /**
+     * Create an mutable(modifiable) list using List.of (JDK 9+)
+     */
+    private static List<String> createMutableListWithListOf() {
+        return new ArrayList<>(List.of("Larson", "Tolkien", "Dubner", "Rothfuss"));
+    }
+
+}
