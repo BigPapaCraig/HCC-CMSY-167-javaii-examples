f6ac54629f83e71ff4d32fd6ba5d40c1ffb7bbd6
Chris Finley <cfinley@howardcc.edu>
2021-03-13 09:45:43 -0500

    Add infinite stream example.

---
+++ lambdas/src/main/java/edu/howardcc/javaii/examples/InfiniteStreamDemo.java
@@ -0,0 +1,18 @@
+package edu.howardcc.javaii.examples;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Demonstrate creating an infinite stream
+ */
+public class InfiniteStreamDemo {
+
+    public static void main(String[] args) {
+
+        // Count by fives with an infinite stream
+        System.out.println(Stream.iterate(5, i -> i + 5).limit(20).collect(Collectors.toList()));
+
+    }
+
+}
